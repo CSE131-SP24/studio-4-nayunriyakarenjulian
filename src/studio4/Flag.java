cb576c7b7fa6cf752066e7013922cb59bd66c91a
nayunkim <nayunkim@localhost>
2024-02-21 15:45:53 -0600

    .

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,18 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setPenColor(47, 147, 247);
+		StdDraw.setPenRadius(0.09);
+		StdDraw.circle(0.5, 0.5, 0.1);
+		
+		StdDraw.setPenColor(50, 70, 80);
+		StdDraw.filledRectangle(0.5, 0.3, 0.5, 0.2);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5, 0.1, 0.5, 0.1);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledRectangle(0.5, 0.9, 0.5, 0.1);
 	}
 }
