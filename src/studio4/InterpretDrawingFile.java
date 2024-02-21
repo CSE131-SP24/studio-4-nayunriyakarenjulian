d8d38bc52772fce38b2fc5ef8bbb5f57a6c685ae
nayunkim <nayunkim@localhost>
2024-02-21 15:13:54 -0600

    .

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,26 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		Color customColor = new Color(red, green, blue);
+		
+		boolean isFilled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		System.out.println(shape);
+		StdDraw.setPenColor(customColor);
+		
+		if (shape.equals ("rectangle")) {
+			StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		} else {
+			StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+		}
 	}
 }
